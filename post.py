427887fa457e6a26b6732bc0ab25a0829ac9f0b0
Grady Roberts <gnr7aj@virginia.edu>
2020-05-28 22:13:50 -0400

    ignore files

--- post.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-from config import probe_key
-
-s = requests.Session()
-payloads = [{'key':probe_key, 'temp_C':100.0, 'temp_F':i} for i in [68.0,70.0,72.0,74.0]]
-for p in payloads:
-    s.post('http://localhost:5000', data=p)
-    time.sleep(30)
